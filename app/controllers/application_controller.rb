022329ff4c53d67f75d78983ab54625adb1b6dd6
rwpr <ruuiwong@gmail.com>
2015-09-01 11:52:51 +0800

    user devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < ActionController::Base
+	
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  include CanCan::ControllerAdditions
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    # permits authentication keys (email) plus password and password_confirmation
+    devise_parameter_sanitizer.for(:account_update) << :name
+    # permits authentication keys (email) plus password, password_confirmation and current_password
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :roles
+  resources :properties
+  resources :propertytags
+  resources :tags
+  resources :books
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
